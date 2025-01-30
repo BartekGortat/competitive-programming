661c3faccdd52235545cef65c894b17fad2abc80
Bartek Gortat <no@e.mail>
2025-01-30 14:27:18 +0100

    Add a task solution.

---
+++ olympiad_in_informatics/pl/30/stage_1/plytkie_nawiasowania.cpp
@@ -0,0 +1,41 @@
+// https://szkopul.edu.pl/problemset/problem/KlznBqTfieN-sWvUfTD0BJdT/site/?key=statement
+
+#include <iostream>
+#include <list>
+#include <stack>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    int n; // character count
+    int h; // depth
+    string brackets;
+    cin >> n >> h >> brackets;
+    int amendment_count = 0;
+    int depth = 0;
+    for (char bracket : brackets)
+    {
+        if (bracket == '(')
+        {
+            ++depth;
+            if (depth > h)
+            {
+                ++amendment_count;
+                depth -= 2;
+            }
+        }
+        else
+        {
+            --depth;
+            if (depth < 0)
+            {
+                ++amendment_count;
+                depth = 1;
+            }
+        }
+    }
+    cout << amendment_count << endl;
+    return 0;
+}
